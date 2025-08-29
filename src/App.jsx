6bcbbbab06857d575a6db3839809cc0927f1d12e
lucaferraria15-hue <lucaferraria15@gmail.com>
2025-08-29 07:32:38 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SOPWizard from "./SOP DE OPTIMIZACIÓN Y ESCALAMIENTO DE EMPRESA";
-import OPSFormPreview from "./OPSFormPreview";
-import FormularioNuevo from "./FormularioNuevo";
+import SOPWizard from "./SOPWizard";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/ops" element={<OPSFormPreview />} />
-        <Route path="/nuevo" element={<FormularioNuevo />} />
-        <Route path="/sop" element={<SOPWizard />} />
-      </Routes>
-    </Router>
-  );
+  return <SOPWizard />;
 }
